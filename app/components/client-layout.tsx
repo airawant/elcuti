4eeb61185dbd031bd071c005cf4262f648dd8af4
airawant <udemyarief@gmail.com>
2026-08-06 08:16:37 +0700

    feat: add ClientLayout component with ThemeProvider, Toaster, and Footer integration

--- app/components/client-layout.tsx
+++ app/components/client-layout.tsx
@@ -3,6 +3,7 @@
 import type React from "react"
 
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 import { Footer } from "./Footer"
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
@@ -14,6 +15,7 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
         </main>
         <Footer />
       </div>
+      <Toaster />
     </ThemeProvider>
   )
 }
